392c62a4b8caa12eea39b59d65e81fd86b8bc0f4
wmaurice-73 <warthmaurice@gmail.com>
2026-03-08 14:27:56 +0100

    Refactor mean excess plot code for clarity

--- src/pot_method/mean_excess_plot.py
+++ src/pot_method/mean_excess_plot.py
@@ -28,25 +28,3 @@ plt.grid(axis='x', which='both', linestyle='-', alpha=0.5)
 plt.grid(True)
 plt.show()
 
-#we want to compare if our choice of u supports the GPD model, hence we add a linear function y_linear = a*u + b to the plot
-#y_linear schould have slope gamma(1-gamma), b has to be adjusted such that the line intersects the mean excess plot at the desired choice of u
-a = -0.244  
-b = 29.9
-y_linear = a * u_vals + b
-
-#plot both functions in one plot
-plt.plot(u_vals, y_linear, lw=0.4, linestyle="--", color = "grey")
-plt.plot(u_vals, y,lw=0.6, color = "black")
-plt.xticks(np.arange(110, 120, 1))
-# Set minor ticks (invisible markers) at 0.25 distance
-ax = plt.gca() # Get Current Axis
-ax.xaxis.set_minor_locator(MultipleLocator(0.25))
-# Turn on the grid for the x-axis, applying it to 'both' major and minor ticks
-plt.grid(axis='x', which='both', linestyle='-', alpha=0.5)
-plt.grid(True)
-plt.tick_params(axis='both', labelsize=7)
-#uncomment the next line if you want to save the plot
-#plt.savefig("./src/pot_method/meanexcess_plot.pdf", bbox_inches="tight")
-plt.show()
-
-
